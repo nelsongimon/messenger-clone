0ec34f8d60fa260d663e5233922f85945a55cbbe
Nelson Gimon <nelson.gimon@gmail.com>
2023-05-30 20:13:15 -0400

    feature/settings-functionality done!

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
 import { useState } from "react";
 import Avatar from "../Avatar";
+import SettingModal from "./SettingModal";
 
 interface DesktopSidebarProps {
   currentUser: User | null;
@@ -16,66 +17,73 @@ export default function DesktopSidebar({
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="
-      hidden
-      lg:fixed
-      lg:inset-y-0
-      lg:left-0
-      lg:z-40
-      lg:w-20
-      lg:overflow-y-auto
-      lg:bg-white
-      lg:border-r-[1px]
-      lg:pb-4
-      lg:flex
-      lg:flex-col
-      justify-between
-    ">
-      <nav className="
-        mt-4
-        flex
-        flex-col
+    <>
+      <SettingModal
+        isOpen={isOpen}
+        currentUser={currentUser}
+        onClose={() => setIsOpen(false)}
+      />
+      <div className="
+        hidden
+        lg:fixed
+        lg:inset-y-0
+        lg:left-0
+        lg:z-40
+        lg:w-20
+        lg:overflow-y-auto
+        lg:bg-white
+        lg:border-r-[1px]
+        lg:pb-4
+        lg:flex
+        lg:flex-col
         justify-between
       ">
-        <ul
-          role="list"
-          className="
-            flex
-            flex-col
-            items-center
-            space-y-1
-          "
-        >
-          {routes.map((item) => (
-            <DesktopItem 
-              key={item.label}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
-              active={item.active}
-              onClick={item.onClick}
-            />
-          ))}
-        </ul>
-      </nav>
-      <nav className="
-        mt-4
-        flex
-        flex-col
-        justify-between
-        items-center
-      ">
-        <div
-          onClick={() => setIsOpen(true)}
-          className="
-            cursor-pointer
-            hover:opacity-75
-            transition
-          "
-        >
-          <Avatar user={currentUser} />
-        </div>
-      </nav>
-    </div>
+        <nav className="
+          mt-4
+          flex
+          flex-col
+          justify-between
+        ">
+          <ul
+            role="list"
+            className="
+              flex
+              flex-col
+              items-center
+              space-y-1
+            "
+          >
+            {routes.map((item) => (
+              <DesktopItem
+                key={item.label}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+                active={item.active}
+                onClick={item.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <nav className="
+          mt-4
+          flex
+          flex-col
+          justify-between
+          items-center
+        ">
+          <div
+            onClick={() => setIsOpen(true)}
+            className="
+              cursor-pointer
+              hover:opacity-75
+              transition
+            "
+          >
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 }
